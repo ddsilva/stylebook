79051a9da580c98ab2c61188aac32dc1a7efcb3c
alan-oliv <alanbruno@gmail.com>
2019-04-10 23:16:18 -0300

    build(Webpack): configuring typescript loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,6 @@ const SRC_PATH = path.join(__dirname, '../src');
 const STORIES_PATH = path.join(__dirname, '../stories');
 
 module.exports = ({ config }) => {
-
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: [SRC_PATH, STORIES_PATH],
