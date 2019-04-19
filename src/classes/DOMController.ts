713fd20bd0492efc5bf600c7c3136ada81d38885
alan-oliv <alanbruno@gmail.com>
2019-04-18 21:34:16 -0300

    fix(Fullscreen): fix on fullscreen render

--- src/classes/DOMController.ts
+++ src/classes/DOMController.ts
@@ -1,4 +1,5 @@
-import * as ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
+import { ReactElement } from 'react'
 import { ManagerData } from '../interfaces'
 import { ManagerComponent } from '../types'
 import {
@@ -16,9 +17,18 @@ class DOMController {
   private stories: object[]
 
   public constructor() {
-    this.root = document.querySelector(ROOT_NODE_SELECTOR)
-    this.observer = new MutationObserver(() => this.setContainerVisible(false))
-    this.root && this.observer.observe(this.root, { childList: true })
+    this.root = document.querySelector(ROOT_NODE_SELECTOR) as Element
+    this.setObserver(this.root, () => this.setContainerVisible(false))
+  }
+
+  private setObserver = (
+    node: Element,
+    callback: MutationCallback,
+    options: object = { childList: true }
+  ): void => {
+    this.observer && this.observer.disconnect()
+    this.observer = new MutationObserver(callback)
+    this.observer.observe(node, options)
   }
 
   private setContainerVisible = (visibility: boolean) => {
@@ -30,31 +40,33 @@ class DOMController {
       )
   }
 
-  public hydrate = (content: ManagerData) => {
-    if (!this.stories) {
+  public hydrate = (content: ManagerData, reRender: boolean = false) => {
+    if (!this.stories || reRender) {
       const {
         stories,
         component
       }: { stories: object[]; component: ManagerComponent } = content
 
+      const {
+        props: { children: newManager }
+      } = component
+
       this.stories = stories
 
       if (this.root) {
         const defaultManager = this.root.querySelector(MANAGER_SELECTOR)
-        const {
-          props: { children: newManager }
-        } = component
+        const container = this.root.querySelector(CONTAINER_SELECTOR) as Element
 
         if (defaultManager) {
           while (defaultManager.firstChild) defaultManager.firstChild.remove()
-
           defaultManager.setAttribute('id', NEW_MANAGER_ID)
-        }
 
-        ReactDOM.render(
-          newManager(stories) as React.ReactElement<object>,
-          document.getElementById(NEW_MANAGER_ID)
-        )
+          ReactDOM.render(
+            newManager(stories) as ReactElement,
+            document.getElementById(NEW_MANAGER_ID)
+          )
+          this.setObserver(container, () => this.hydrate(content, true))
+        }
         this.setContainerVisible(true)
       }
     }
