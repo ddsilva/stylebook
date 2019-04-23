67f7573456e90f8a21b56a76e174bbee763e35bb
alan-oliv <alanbruno@gmail.com>
2019-04-22 21:55:59 -0300

    feat(Observers): improved observers

--- src/classes/DOMController.ts
+++ src/classes/DOMController.ts
@@ -9,8 +9,13 @@ import {
   PREVIEW_SELECTOR
 } from '../constants'
 
+interface Overseer {
+  name: string
+  observer: MutationObserver
+}
+
 class DOMController {
-  private observer: MutationObserver
+  private observers: Overseer[] = []
 
   private root: Element
 
@@ -18,60 +23,101 @@ class DOMController {
 
   public constructor() {
     this.root = document.querySelector(ROOT_NODE_SELECTOR) as Element
-    this.setObserver(this.root, () => this.setContainerProperties(false))
+    this.setObserver('root', this.root, () =>
+      this.setContainerProperties(false)
+    )
   }
 
   private toggleFullscreen = () => {
     this.fullscreen = !this.fullscreen
   }
 
+  private getObserver = (name: string) =>
+    this.observers.find(item => item.name === name)
+
   private setObserver = (
+    name: string,
     node: Element,
     callback: MutationCallback,
     options: object = { childList: true }
   ): void => {
-    this.observer && this.observer.disconnect()
-    this.observer = new MutationObserver(callback)
-    this.observer.observe(node, options)
+    const MutationOverseer: Overseer = {
+      name,
+      observer: new MutationObserver(callback)
+    }
+    MutationOverseer.observer.observe(node, options)
+    this.observers.push(MutationOverseer)
   }
 
   private setContainerProperties = (visibility: boolean) => {
     if (visibility) {
       this.containerPositioning()
     } else {
-      const container = this.root.querySelector(
+      const container: HTMLElement = this.root.querySelector(
         CONTAINER_SELECTOR
       ) as HTMLElement
 
-      container && (container.style.visibility = 'hidden')
+      container.style.visibility = 'hidden'
     }
   }
 
   private containerPositioning = () => {
-    this.handleDraggables()
+    this.hideDraggables()
 
-    const manager = this.root.querySelector(MANAGER_SELECTOR)
-    const preview = this.root.querySelector(PREVIEW_SELECTOR)
-    const container = this.root.querySelector(CONTAINER_SELECTOR) as HTMLElement
-    const toggleFs = document.querySelector(
+    const manager: HTMLElement = this.root.querySelector(
+      MANAGER_SELECTOR
+    ) as HTMLElement
+
+    const preview: HTMLElement = this.root.querySelector(
+      PREVIEW_SELECTOR
+    ) as HTMLElement
+
+    const container: HTMLElement = this.root.querySelector(
+      CONTAINER_SELECTOR
+    ) as HTMLElement
+
+    const toggleFs: Element = document.querySelector(
       'button[title="Go full screen"], button[title="Exit full screen"]'
     ) as Element
 
     if (manager && preview && container) {
-      toggleFs.addEventListener('click', this.toggleFullscreen)
+      toggleFs && toggleFs.addEventListener('click', this.toggleFullscreen)
+
       manager.setAttribute('style', 'position: relative; width: auto')
       preview.setAttribute('style', 'position: relative')
+
+      this.setObserver(
+        'preview',
+        preview,
+        mutation =>
+          this.avoidMutation(mutation, 'attributes', () => {
+            // remove automatically injected attribute
+          }),
+        { childList: true, attributes: true }
+      )
+
       container.style.transition = ''
       container.style.display = 'flex'
       container.style.visibility = 'visible'
     }
   }
 
-  private handleDraggables = () => {
+  private avoidMutation = (
+    mutations: MutationRecord[],
+    type: string,
+    callback: any
+  ) => {
+    const attributeChanged = mutations.some(mutation => mutation.type === type)
+
+    if (attributeChanged) {
+      callback()
+    }
+  }
+
+  private hideDraggables = () => {
     const draggables = Array.from(
       this.root.querySelectorAll('.react-draggable')
     )
-
     draggables &&
       draggables.map((draggable: HTMLElement) =>
         draggable.setAttribute('style', 'display: none')
@@ -81,7 +127,6 @@ class DOMController {
   public hydrate = (content: ManagerData) => {
     if (!this.fullscreen) {
       console.log('hydrate')
-
       const {
         stories,
         component
@@ -92,12 +137,15 @@ class DOMController {
       } = component
 
       if (this.root) {
-        const defaultManager = this.root.querySelector(
+        const defaultManager: Element = this.root.querySelector(
           MANAGER_SELECTOR
         ) as Element
 
-        const container = this.root.querySelector(CONTAINER_SELECTOR) as Element
-        let managerPlaceHolder = defaultManager.querySelector(
+        const container: Element = this.root.querySelector(
+          CONTAINER_SELECTOR
+        ) as Element
+
+        let managerPlaceHolder: Element = defaultManager.querySelector(
           '#new-root'
         ) as Element
 
@@ -107,6 +155,7 @@ class DOMController {
           managerPlaceHolder = defaultManager.querySelector(
             '#new-root'
           ) as Element
+
           ReactDOM.render(
             newManager(stories) as ReactElement,
             managerPlaceHolder
@@ -114,7 +163,8 @@ class DOMController {
         }
 
         this.setContainerProperties(true)
-        this.setObserver(container, () => this.hydrate(content))
+        !this.getObserver('container') &&
+          this.setObserver('container', container, () => this.hydrate(content))
       }
     }
   }
